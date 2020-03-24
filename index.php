41aaa8fca8175eaea89d73446d8f4aa3e9f7dc43
Arnaud BODEL <contact@osw3.net>
2020-03-24 10:45:08 +0100

    -

--- index.php
+++ index.php
@@ -4,3 +4,29 @@
  * --
  * The main template file
  */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    
+    <!-- #main-header -->
+    <header id="main-header" class="main-header">
+        
+    </header><!-- End #main-header -->
+
+    <!-- #main-content -->
+    <div id="main-content" class="main-content">
+
+    </div><!-- End #main-content -->
+
+    <!-- #main-footer -->
+    <footer id="main-footer" class="main-footer">
+        
+    </footer><!-- End #main-footer -->
+
+</body>
+</html>

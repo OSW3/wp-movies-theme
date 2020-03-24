8a9f5e094b74a2425517fdff12a1cc797e3adb16
Arnaud BODEL <contact@osw3.net>
2020-03-24 16:09:29 +0100

    -

---
+++ templates/my-homepage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: My Homepage Tmpl
+ */
+?>
+<?php get_header() ?>
+
+<div class="container">
+    <h1>Bienvenue sur mon site <small><?= bloginfo('name') ?></small> </h1>
+</div>
+
+<?php get_footer() ?>

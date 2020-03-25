56e1ccb12b876395a892285971b6468feab21322
Arnaud BODEL <contact@osw3.net>
2020-03-25 10:28:51 +0100

    -

--- functions/custompost_movie.php
+++ functions/custompost_movie.php
@@ -9,17 +9,17 @@ function create_post_movie()
 
     $labels = [
         'name'                  => $post_label,
-        // 'singular_name'         => "",
-        // 'menu_name'             => "",
+        'singular_name'         => "Film",
+        'menu_name'             => $post_label,
         'name_admin_bar'        => "Les films",
         // 'parent_item_colon'     => "",
         // 'all_items'             => "",
-        // 'add_new_item'          => "",
-        // 'add_new'               => "",
-        // 'new_item'              => "",
+        'add_new_item'          => "Ajout d'un film",
+        // 'add_new'               => "Ajouter un film",
+        // 'new_item'              => "Ajout d'un film",
         // 'edit_item'             => "",
         // 'update_item'           => "",
-        // 'view_item'             => "",
+        'view_item'             => "Un film",
         // 'search_items'          => "",
         // 'not_found'             => "",
         // 'not_found_in_trash'    => "",
@@ -43,6 +43,7 @@ function create_post_movie()
 
         'description'   => "Mes critique de films",
         'labels'        => $labels,
+        'supports'      => ['title', 'editor', 'custom-fields'],
     ];
 
     register_post_type($post_type, $args);
---
+++ templates/my-movies.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * Template Name: Template Movies
+ */
+
+?>
+<?php get_header() ?>
+<div class="container">
+    <h1>xxxxxx</h1>
+
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Titre</th>
+                <th>Critique</th>
+                <th>Voir</th>
+            </tr>
+        </thead>
+        <tbody>
+        
+            <tr>
+                <td>xxx</td>
+                <td>xxx</td>
+                <td>xxx</td>
+            </tr>
+
+        </tbody>
+    </table>
+</div>
+<?php get_footer() ?>

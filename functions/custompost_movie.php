4e9c679c2e41710c52e80361129a37ba04f0f5ba
Arnaud BODEL <contact@osw3.net>
2020-03-25 14:33:27 +0100

    -

--- functions/custompost_movie.php
+++ functions/custompost_movie.php
@@ -42,9 +42,27 @@ function create_post_movie()
 
         'description'   => "Mes critique de films",
         'labels'        => $labels,
-        'supports'      => ['title', 'editor', 'custom-fields'],
+        'supports'      => ['title', 'editor'],
     ];
 
     register_post_type($post_type, $args);
 }
 add_action('init', "create_post_movie");
+
+
+function create_custom_metabox()
+{
+    add_meta_box(
+        "my_custom_metabox",    // ID de la metabox
+        "Titre de la metabox",  // Titre
+        "show_my_custom_fields" // Callback
+    );
+}
+add_action('add_meta_boxes', "create_custom_metabox");
+
+function show_my_custom_fields($post)
+{
+    ?>
+    <input type="text" name="director" value="<?= get_post_meta($post->ID, 'director', true) ?>">
+    <?php
+}
